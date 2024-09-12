1c4a3771fe95c4dd4d262fd1030bc06b3384e5e8
SafyaAbouelfadel <safia.abf@gmail.com>
2024-09-12 08:32:37 -0700

    imports all functions from a file and handles basic operations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     a = int(sys.argv[1])
     b = int(sys.argv[3])
     op = sys.argv[2]
-    
+
     if op == "+":
         print("{} + {} = {}".format(a, b, add(a, b)))
     elif op == "-":
