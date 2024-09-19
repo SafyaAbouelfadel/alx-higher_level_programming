e6544dea6ffede08c7c9a685380bee1bd21a1fe2
SafyaAbouelfadel <safia.abf@gmail.com>
2024-09-18 18:21:45 -0700

    Technical interview: converts a Roman numeral to an integer.

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,5 +7,5 @@ def roman_to_int(roman_string):
     roman = {'I': 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     for k in reversed(roman_string):
         number = roman[k]
-        result += number if total < number * 5 else -number
+        result += number if result < number * 5 else -number
     return result
