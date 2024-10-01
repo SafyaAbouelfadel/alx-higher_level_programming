fb9248a945734f08209f29d4c6670dd5b916b29c
SafyaAbouelfadel <safia.abf@gmail.com>
2024-10-01 15:55:25 -0700

    square with a class

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -64,6 +64,6 @@ class Square:
         for k in range(0, self.__size):
             [print(" ", end="") for c in range(0, self.__position[0])]
             [print("#", end="") for b in range(0, self.__size)]
-            if i != self.__size - 1:
+            if k != self.__size - 1:
                 print("")
         return ("")
