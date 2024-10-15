599d0df373ee8d988497d75b1109c7e391ef6aeb
SafyaAbouelfadel <safia.abf@gmail.com>
2024-10-14 17:09:30 -0700

    python-inheritance fix style

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
-'''Module for is_same_class method.'''
+"""
+Module for is_same_class function.
+"""
 
 
 def is_same_class(obj, a_class):
-    '''Determines if an object is exactly an instance of a class.'''
-    return (type(obj) == a_class)
+    """
+    Determines if an object is exactly an instance of a specified class.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to compare against.
+
+    Returns:
+        True if obj is exactly an instance of a_class, otherwise False.
+    """
+    return type(obj) == a_class
