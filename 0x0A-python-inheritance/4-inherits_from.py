6ccffd8daf38f31c08324970161b11ef50a40c1e
SafyaAbouelfadel <safia.abf@gmail.com>
2024-10-14 17:14:39 -0700

    python-inheritance fix style

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,4 +6,4 @@ Module for inherits_from method
 
 def inherits_from(obj, a_class):
     '''Determines if an obj is a true subclass of a class'''
-    return isinstance(obj, a_class) and type(obj) != a_class
+    return isinstance(obj, a_class) and type(obj) is not a_class
